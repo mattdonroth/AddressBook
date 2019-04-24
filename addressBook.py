50806381a18bb5a68eb1ddaf46de647e019c968b
jobeSoffa <otisjobe123@gmail.com>
2019-04-23 18:39:36 -0700

    Update addressBook.py

--- addressBook.py
+++ addressBook.py
@@ -1,8 +1,69 @@
 import argparse
+import re
 
 class address_book:
     def __init__(self):
         self.address_book = {}
+
+    def isEmail(self,number):
+        variable = 1
+
+        regex = "[^@]+@[^@]+\.[^@]+"
+        if re.search(regex, number) and len(number) < 320:
+            print("valid")
+            return (True)
+
+        else:
+            print("not valid")
+            return False
+
+    def isAddress(self,number):
+        if len(number) < 60:
+            print("valid")
+            return (True)
+
+        else:
+            print("not valid")
+            return False
+    def isZipCode(self,number):
+        number = number.replace("-", "")
+        number = number.replace(" ", "")
+
+        if len(number) < 16 and number.isdigit() and len(number) > 4:
+            print("valid")
+            return (True)
+
+        else:
+            print("not valid")
+            return False
+    def isPhoneNumber(self,number):
+        variable = 1
+
+        regex = "\w{3}-\w{3}-\w{4}"
+
+        if re.search(regex, number):
+            print("valid number")
+            return True
+        else:
+            number = number.replace("-", "")
+            number = number.replace("(", "")
+            number = number.replace(")", "")
+            number = number.replace(" ", "")
+            if (len(number) == 10) and number.isdigit():
+                print("valid")
+                return True
+            else:
+                print("invalid")
+                return False
+
+    def isName(self,number):
+        if len(number) < 60:
+            print("valid")
+            return (True)
+
+        else:
+            print("not valid")
+            return False
 
     def record_add(self, Name, address):
         if len(self.address_book) > 5:
@@ -24,11 +85,12 @@ class address_book:
             return out
         except:
             return None
-    
+
     def old_shell(self):
         flag = True
         while flag:
-            seel = raw_input("Please Select An Option:\n1. Get a Record\n2. Add a Record\n3. Delete a Record\n4. Exit\n")
+            seel = input(
+                "Please Select An Option:\n1. Get a Record\n2. Add a Record\n3. Delete a Record\n4. Exit\n")
             try:
                 sel = int(seel)
             except:
@@ -37,15 +99,15 @@ class address_book:
             if sel > 5:
                 print("Please enter a valid selection")
             elif sel == 1:
-                arg = raw_input("Please enter a name to lookup:")
+                arg = input("Please enter a name to lookup:")
                 out = self.record_get(arg)
                 if out == None:
                     print("Record Not Found")
                 else:
                     print(out)
             elif sel == 2:
-                arg1 = raw_input("Please enter the Name to add:")
-                arg2 = raw_input("Please enter the Address to add:")
+                arg1 = input("Please enter the Name to add:")
+                arg2 = input("Please enter the Address to add:")
                 if type(arg1) != str or type(arg2) != str:
                     print("Please enter a valid input")
                 else:
@@ -55,7 +117,7 @@ class address_book:
                     elif out == True:
                         print("Record added")
             elif sel == 3:
-                arg1 = raw_input("Please enter the Name to remove:")
+                arg1 = input("Please enter the Name to remove:")
                 out = self.record_del(arg1)
                 if out == True:
                     print("Record Removed")
@@ -65,11 +127,12 @@ class address_book:
                 flag = False
                 print("Thanks for using the most advanced address book system in the world. Have a nice day!")
 
+
 if __name__ == "__main__":
-    '''
+
     this = address_book()
     this.old_shell()
-    '''
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-ADD", help="Add a user to the Address Book")
     parser.add_argument("-DEL", help="Delete a user from the Address Book")
@@ -77,4 +140,4 @@ if __name__ == "__main__":
     parser.add_argument("-UPDATE", help="Update an existing user in the Address Book")
     parser.add_argument("-LIST", help="List all users in the Address Book")
     args = parser.parse_args()
-    print(args.echo)
+    print(args)
