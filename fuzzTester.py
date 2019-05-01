9a166954b7ddcab92b9899a8409d6a630258b14c
MRoth <mroth@sandiego.edu>
2019-04-30 18:39:51 -0700

    Getting Fuzzy, Odd Behavior

--- fuzzTester.py
+++ fuzzTester.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+import random
+import string
+import time
 
 test_commands = ['2',
                 'fuzz',
@@ -36,10 +39,73 @@ test_commands = ['2',
 
 p = subprocess.Popen("python addressBook.py", shell=True, stderr=subprocess.PIPE, 
                      stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+x = 0
+y = 0
+#Login
+numLoginAttempts = random.randint(1, 255)
+while x < numLoginAttempts:
+    randNumArgs = random.randint(1, 90)
+    ranStr = ''
+    while y < randNumArgs:
+        randStrLen = random.randint(1, 255)
+        ranStr+= ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(randStrLen))
+        ranStr+= " "
+        y+=1
+    test_commands.append("LOGIN "+ranStr)
+    x+=1
+
+x = 0
+y = 0
+#Get
+numGetAttempts = random.randint(1, 255)
+while x < numGetAttempts:
+    randNumArgs = random.randint(1, 90)
+    ranStr = ''
+    while y < randNumArgs:
+        randStrLen = random.randint(1, 255)
+        ranStr+= ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(randStrLen))
+        ranStr+= " "
+        y+=1
+    test_commands.append("GET "+ranStr)
+    x+=1
+
+x = 0
+y = 0
+#Add
+numAddAttempts = random.randint(1, 255)
+while x < numAddAttempts:
+    randNumArgs = random.randint(1, 90)
+    ranStr = ''
+    while y < randNumArgs:
+        randStrLen = random.randint(1, 255)
+        ranStr+= ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(randStrLen))
+        ranStr+= " "
+        y+=1
+    test_commands.append("ADD "+ranStr)
+    x+=1
+
+x = 0
+y = 0
+#Del
+numDelAttempts = random.randint(1, 255)
+while x < numDelAttempts:
+    randNumArgs = random.randint(1, 90)
+    ranStr = ''
+    while y < randNumArgs:
+        randStrLen = random.randint(1, 255)
+        ranStr+= ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(randStrLen))
+        ranStr+= " "
+        y+=1
+    test_commands.append("DEL "+ranStr)
+    x+=1
+
 for test_command in test_commands:
+    print(test_command)
+    '''
     p.stdin.write(test_command.encode())
     p.stdin.write('\n'.encode())
     p.stdin.flush()
+    '''
 
 p.stdin.close()
 for line in p.stdout.readlines():
